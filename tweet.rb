2e435ce5f6c0891890525585449c190731cac86a
gilmae <avocadia@gmail.com>
2016-12-02 17:08:35 +1100

    Attach to AWS for the files

--- tweet.rb
+++ tweet.rb
@@ -1,6 +1,8 @@
 require 'twitter'
 require 'YAML'
+require 'JSON'
 require 'fileutils'
+require 'aws-sdk'
 
 config_file = File.expand_path(File.dirname(__FILE__)) + '/.config'
 
@@ -20,19 +22,19 @@ base_path = config["images"]
 # the files we are interested in are in th format:
 #   mb_real_imaginary_zoom.[png/jpg]
 
-mb_file_regex = /mb_([^_]+)_([^_]+)_([^_]+)\./
+#mb_file_regex = /mb_([^_]+)_([^_]+)_([^_]+)\./
 
-next_mb = Dir.entries(base_path).sort_by { |a| File.mtime(base_path + a) }.find do |a|
-  mb_file_regex =~ a;
-end
+#next_mb = Dir.entries(base_path).sort_by { |a| File.mtime(base_path + a) }.find do |a|
+#  mb_file_regex =~ a;
+#end
 
-exit if next_mb.nil?
+#exit if next_mb.nil?
 
-m = next_mb.match(mb_file_regex)
+#m = next_mb.match(mb_file_regex)
 
-real = m.captures[0]
-imaginary = m.captures[1]
-zoom = m.captures[2]
+#real = m.captures[0]
+#imaginary = m.captures[1]
+#zoom = m.captures[2]
 
 
 if !config["twitter"]
@@ -40,18 +42,58 @@ if !config["twitter"]
   exit
 end
 
-# configure up the external services
-client = Twitter::REST::Client.new do |twitter|
-    twitter.consumer_key = config["twitter"]["CONSUMER_KEY"]
-    twitter.consumer_secret = config["twitter"]["CONSUMER_SECRET"]
-    twitter.access_token = config["twitter"]["OAUTH_TOKEN"]
-    twitter.access_token_secret = config["twitter"]["OAUTH_TOKEN_SECRET"]
-  end
-
-"Snapshot taken#{real} + #{imaginary}i at zoom #{zoom}"
-
-File.open(base_path + next_mb, "r") do |file|
-   client.update_with_media("#{real} + #{imaginary}i at zoom #{zoom}", file, {:lat=>imaginary, :long=>real, :display_coordinates=>'true'})
+if !config["sqs"]
+  p "AWS SQS configuration missing"
+  exit
 end
 
-FileUtils.mv(base_path + next_mb, base_path + "posted/" + next_mb)
+if !config["s3"]
+  p "AWS S3 configuration missing"
+  exit
+end
+
+sqs_credentials = Aws::Credentials.new(config["sqs"]["access_key"], config["sqs"]["secret_key"])
+s3_credentials = Aws::Credentials.new(config["s3"]["access_key"], config["s3"]["secret_key"])
+
+sqs = Aws::SQS::Client.new(region:  config["sqs"]["region"], credentials: sqs_credentials)
+
+s3 = Aws::S3::Resource.new(region: config["s3"]["region"], credentials: s3_credentials)
+
+get_queue_response = sqs.get_queue_url(queue_name: config["sqs"]["queue_name"])
+
+receive_resp = sqs.receive_message({queue_url: get_queue_response.queue_url, max_number_of_messages: 1})
+
+exit if (receive_resp.nil? || receive_resp.messages.nil? || receive_resp.messages.size == 0)
+
+msg = receive_resp.messages.first
+
+payload = JSON.load(msg.body)
+
+key = payload["key"]
+filepath = '/tmp/#{key}'
+
+bucket = s3.bucket(config["s3"]["bucket_name"])
+obj = bucket.object(key)
+obj.get({response_target: filepath})
+
+#configure up the external services
+client = Twitter::REST::Client.new do |twitter|
+   twitter.consumer_key = config["twitter"]["CONSUMER_KEY"]
+   twitter.consumer_secret = config["twitter"]["CONSUMER_SECRET"]
+   twitter.access_token = config["twitter"]["OAUTH_TOKEN"]
+   twitter.access_token_secret = config["twitter"]["OAUTH_TOKEN_SECRET"]
+ end
+
+File.open(filepath, "r") do |file|
+  client.update_with_media("#{payload["real"]} + #{payload["imaginary"]}i at zoom #{payload["zoom"]}", file, {:lat=>payload["imaginary"], :long=>payload["real"], :display_coordinates=>'true'})
+end
+
+FileUtils.rm(filepath)
+
+new_key = "#{config["s3"]["posted_folder"]}/#{key}"
+obj.move_to(bucket.object(new_key))
+
+resp = sqs.delete_message({
+  queue_url: get_queue_response.queue_url,
+  receipt_handle: msg.receipt_handle
+})
