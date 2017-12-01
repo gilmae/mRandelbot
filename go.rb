b5fd455379a4e025a99491fc1559ae7872071bb1
gilmae <avocadia@fastmail.fm>
2017-12-01 20:41:08 +1100

    Now tweets.

--- go.rb
+++ go.rb
@@ -1,3 +1,4 @@
+require 'twitter'
 require 'YAML'
 require 'JSON'
 require 'fileutils'
@@ -45,6 +46,18 @@ def get_a_point m, real, imaginary, zoom
     return parsed_coords[0], parsed_coords[1]
 end
 
+def add_meta_data filename, exiftool, real, imag, zoom
+    
+      `#{exiftool} -gps:GPSLongitude="#{real}" #{filename}`
+      `#{exiftool} -gps:GPSLongitudeRef="W" #{filename}` if real.to_f < 0
+    
+      `#{exiftool} -gps:GPSLatitude="#{imag}" #{filename}`
+      `#{exiftool} -gps:GPSLatitudeRef="S" file` if imag.to_f < 0
+    
+      `#{exiftool} -DigitalZoomRatio="#{zoom}" #{filename}`
+      `#{exiftool} exiftool -delete_original! #{filename}`
+end
+
 m = Mrandelbot.new
 
 a = m.get_album
@@ -66,12 +79,32 @@ else
     real, imaginary = get_a_point m, r, i, zoom
 end
 
-plot = {zoom: zoom, coords: [real, imaginary]}
+plot = {zoom: zoom, coords: [real, imaginary], published: false}
 a[:points] << plot
 m.save_album a
 
 filename = `#{m.config["mandelbrot"]} -z=#{zoom} -r=#{real} -i=#{imaginary} -c=true -o=#{base_path} -g='#{a[:gradient]}'`.chomp
 a[:points].last[:filename] = filename
+m.save_album a
+
+add_meta_data filename, m.config["exiftool_path"], real, imaginary, zoom
+
+if m.config["mode"] != "DEV"
+    client = Twitter::REST::Client.new do |twitter|
+        twitter.consumer_key = m.config["twitter"]["CONSUMER_KEY"]
+        twitter.consumer_secret = m.config["twitter"]["CONSUMER_SECRET"]
+        twitter.access_token = m.config["twitter"]["OAUTH_TOKEN"]
+        twitter.access_token_secret = m.config["twitter"]["OAUTH_TOKEN_SECRET"]
+    end
+ 
+    File.open(filename, "r") do |file|
+        tweet = client.update_with_media("#{real} + #{imaginary}i at zoom #{zoom}", file, {:lat=>imaginary, :long=>real, :display_coordinates=>'true'})
+        a[:points].last[:tweet] = tweet.id
+        m.save_album a
+    end
+end
+
+a[:points].last[:published] = true
 m.save_album a
 
 
