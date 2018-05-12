8c97a48c681f58f832b9377eae79c2023574c3b1
gilmae <avocadia@fastmail.fm>
2018-05-12 21:40:07 +1000

    Allow the ruby script to handle multiple albums, picking at random from one (or creating a new one)
    This allows the script to replicate the random publish behaviour of the (currently in production) version due to it's reliance on AWS SQS.
    
    A significant amount of refactoring was also done to move as much logic as possible into small functions

--- mRandelbot.rb
+++ mRandelbot.rb
@@ -31,37 +31,15 @@ class Mrandelbot
      end
    end
 
-   def save_album runsheet
-    album_file = File.expand_path(File.dirname(__FILE__)) + '/current.album'
-    File.open(album_file, 'w') do|f|
-      f.write(runsheet.to_json)
-    end
-   end
 
-   def archive_album runsheet
-    album_file = File.expand_path(File.dirname(__FILE__)) + '/current.album'
-    archived_album_file = File.expand_path(File.dirname(__FILE__)) + '/' + runsheet[:album] + '.album.archived'
 
-    File.rename(album_file, archived_album_file)
-   end
 
-   def create_album
-     {run_at: DateTime.now, album: DateTime.now.strftime("%Y%m%d%H%M%S"), points: [], gradient: generate_gradient}
-   end
 
-   def get_album
-    album_file = File.expand_path(File.dirname(__FILE__)) + '/current.album'
-    _album = create_album
 
-    if File.exists? album_file
-      _album = File.open(album_file, 'r') do|f|
-        _album = JSON.parse(f.read)
-      end
-    end
-    save_album _album
-    _album.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-    
-  end
+
+
    
   
 end
+
+
